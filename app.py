83f267aa5539a6d31ad09bd007b85eb3a10f937b
Deekshith kumar <kattadeekshith15@gmail.com>
2025-09-30 17:15:14 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/summarize', methods=['POST'])
+def summarize():
+    if request.method == 'POST':
+        text = request.form['text']
+        summary = summarize_text(text)
+        return render_template('index.html', summary=summary, text=text)
+    return render_template('index.html')
+
+def summarize_text(text, sentence_count=5):
+    parser = PlaintextParser.from_string(text, Tokenizer("english"))
+    summarizer = LsaSummarizer()
+    summary = summarizer(parser.document, sentence_count)
+    return " ".join([str(sentence) for sentence in summary])
+
+if __name__ == '__main__':
+    app.run(debug=True)
